6efa5f7c5f6f6b2ec8e8b8d2e2bce06414836dcc
Simon Dean <simon@simondean.org>
2021-09-12 20:47:51 +0100

    Rename outputs (#3)

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "aws_region" {
   description = "AWS region"
+  default = "us-west-1"
 }
 
 variable "microk8s_instance_type" {
   description = "Type of EC2 instance to provision"
+  default = "t2.micro"
 }
 
 variable "microk8s_instance_name" {
   description = "EC2 instance name"
+  default = "microk8s"
 }
 
