b8b74fe5d515c191fae630b3feb8bb5bdfda43cd
Simon Dean <simon@simondean.org>
2021-09-14 22:41:23 +0100

    Use HTTPS

--- main.tf
+++ main.tf
@@ -152,8 +152,8 @@ resource "aws_security_group" "microk8s_public_subnet" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [var.public_subnet_cidr_block]
   }
