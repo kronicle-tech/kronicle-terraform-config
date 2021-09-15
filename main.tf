b160ab3ed8290d06c6671681aca5ce1725225a73
Simon Dean <simon@simondean.org>
2021-09-15 21:14:46 +0100

    Add CloudWatch logging IAM policy (#16)

--- main.tf
+++ main.tf
@@ -3,49 +3,54 @@ provider "aws" {
 }
 
 resource "aws_vpc" "demo" {
-  cidr_block = var.vpc_cidr_block
-
   tags = {
     Name = "demo"
     "kronicle:terraform" = "true"
   }
+
+  cidr_block = var.vpc_cidr_block
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.demo.id
-  cidr_block              = var.public_subnet_cidr_block
-  availability_zone       = var.public_subnet_az
-
   tags = {
     Name = "public"
     "kronicle:terraform" = "true"
   }
+
+  vpc_id = aws_vpc.demo.id
+  cidr_block = var.public_subnet_cidr_block
+  availability_zone = var.public_subnet_az
 }
 
 resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.demo.id
-
   tags = {
     Name = "main"
     "kronicle:terraform" = "true"
   }
+
+  vpc_id = aws_vpc.demo.id
 }
 
 resource "aws_default_route_table" "default" {
+  tags = {
+    Name = "default"
+    "kronicle:terraform" = "true"
+  }
+
   default_route_table_id = aws_vpc.demo.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-
-  tags = {
-    Name = "default"
-    "kronicle:terraform" = "true"
-  }
 }
 
 data "aws_ami" "ubuntu" {
+  tags = {
+    Name = "ubuntu"
+    "kronicle:terraform" = "true"
+  }
+
   most_recent = true
 
   filter {
@@ -61,14 +66,71 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_security_group" "wireguard_public_internet" {
-  description = "Allow traffic from internet from WireGuard peers"
-  vpc_id      = aws_vpc.demo.id
+resource "aws_iam_role" "ec2_cloudwatch_logging" {
+  tags = {
+    Name = "ec2_cloudwatch_logging"
+    "kronicle:terraform" = "true"
+  }
 
+  name               = "cloudwatch_logging"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17"
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole"
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow"
+        "Sid": ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "cloudwatch_logging" {
+  name        = "cloudwatch_logging"
+  path        = "/"
+  description = "Allows sending logs to CloudWatch"
+
+  policy = jsonencode({
+    "Version": "2012-10-17"
+    "Statement": [
+      {
+        "Effect": "Allow"
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ]
+        "Resource": [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ec2_cloudwatch_logging" {
+  name       = "ec2_cloudwatch_logging"
+  roles      = [aws_iam_role.ec2_cloudwatch_logging.name]
+  policy_arn = aws_iam_policy.cloudwatch_logging.arn
+}
+
+resource "aws_iam_instance_profile" "cloudwatch_logging" {
+  name  = "cloudwatch_logging"
+  role = aws_iam_role.ec2_cloudwatch_logging.name
+}
+
+resource "aws_security_group" "wireguard_public_internet" {
   tags = {
     Name                 = "wireguard_public_internet"
     "kronicle:terraform" = "true"
   }
+
+  description = "Allow traffic from internet from WireGuard peers"
+  vpc_id      = aws_vpc.demo.id
 
   ingress {
     from_port   = var.wireguard_listen_port
@@ -86,13 +148,13 @@ resource "aws_security_group" "wireguard_public_internet" {
 }
 
 resource "aws_security_group" "wireguard_internal" {
-  description = "Allow traffic to internal resources from Wireguard peers"
-  vpc_id      = aws_vpc.demo.id
-
   tags = {
     Name                 = "wireguard_internal"
     "kronicle:terraform" = "true"
   }
+
+  description = "Allow traffic to internal resources from Wireguard peers"
+  vpc_id      = aws_vpc.demo.id
 
   ingress {
     from_port       = 0
@@ -117,21 +179,22 @@ resource "aws_security_group" "wireguard_internal" {
 }
 
 resource "aws_instance" "wireguard" {
+  tags = {
+    Name = "wireguard"
+    "kronicle:terraform" = "true"
+  }
+
   ami                         = data.aws_ami.ubuntu.id
   availability_zone           = var.public_subnet_az
   subnet_id                   = aws_subnet.public.id
   instance_type               = var.wireguard_instance_type
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.cloudwatch_logging.name
   vpc_security_group_ids      = [
     aws_security_group.wireguard_public_internet.id, aws_security_group.wireguard_internal.id]
 
   credit_specification {
     cpu_credits = var.wireguard_cpu_credits
-  }
-
-  tags = {
-    Name = "wireguard"
-    "kronicle:terraform" = "true"
   }
 
   user_data = templatefile("${path.cwd}/wireguard-install-script.sh.tpl", {
@@ -144,13 +207,13 @@ resource "aws_instance" "wireguard" {
 }
 
 resource "aws_security_group" "microk8s_public_subnet" {
-  description = "Allow traffic from other private IP addresses in public subnet"
-  vpc_id      = aws_vpc.demo.id
-
   tags = {
     Name                 = "microk8s_public_subnet"
     "kronicle:terraform" = "true"
   }
+
+  description = "Allow traffic from other private IP addresses in public subnet"
+  vpc_id      = aws_vpc.demo.id
 
   ingress {
     from_port   = 80
@@ -175,19 +238,20 @@ resource "aws_security_group" "microk8s_public_subnet" {
 }
 
 resource "aws_instance" "microk8s" {
+  tags = {
+    Name = "microk8s"
+    "kronicle:terraform" = "true"
+  }
+
   ami                    = data.aws_ami.ubuntu.id
   availability_zone      = var.public_subnet_az
   subnet_id              = aws_subnet.public.id
   instance_type          = var.microk8s_instance_type
+  iam_instance_profile   = aws_iam_instance_profile.cloudwatch_logging.name
   vpc_security_group_ids = [aws_security_group.microk8s_public_subnet.id]
 
   credit_specification {
     cpu_credits = var.microk8s_cpu_credits
-  }
-
-  tags = {
-    Name = "microk8s"
-    "kronicle:terraform" = "true"
   }
 
   user_data = templatefile("${path.cwd}/microk8s-install-script.sh.tpl", {
